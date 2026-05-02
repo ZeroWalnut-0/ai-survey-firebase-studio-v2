ea85a16871d4ce25bdb18db0be3b7cdde277018c
Walnut Zero (호두아님) <tlsgheh@gmail.com>
2026-05-02 02:11:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8932018472-f3348",
+  "appId": "1:1083501362498:web:47f93d2945f0ca93975cdb",
+  "apiKey": "AIzaSyCqc4aNP77W2b7aWiGwOfdzUk239c_9pJk",
+  "authDomain": "studio-8932018472-f3348.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1083501362498"
+};
